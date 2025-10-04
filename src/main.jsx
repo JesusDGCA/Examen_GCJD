e527f68ca352ef40ca4917d9fd6e7e8cd3f4e664
JesusDGCA <jesus.david.gutierrez.carranza@gmail.com>
2025-10-03 18:32:05 -0600

    error commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,13 @@ import './index.css';
 import App from './App.jsx';
 import Navbar from './Navbar/Navbar.jsx';
 import Carrusel from './Carrusel/Carrusel.jsx';
-import CRUD from './CRUDdeEmpleados/CRUD.jsx';
-
+import Crud from './CRUDdeEmpleados/CRUD.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
     <Carrusel />
     <App />
-    <CRUD />
+    <Crud />
   </StrictMode>
 );
